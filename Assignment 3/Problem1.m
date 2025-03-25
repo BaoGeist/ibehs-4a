874b2db7370fd0e24b30f3b2cb865e0a64516b9e
BaoGeist <linofbaoze@outlook.com>
2025-03-24 21:38:28 -0400

    feat: add 1.10

--- Assignment 3/Problem1.m
+++ Assignment 3/Problem1.m
@@ -42,41 +42,39 @@ saveas(gcf, 'Figures/figure1_5.png');
 close_system(modelName, 0);
 
 %% Problem 1.9
-% Set simulation settings
-tauI_values = [0.01, 0.05, 0.1, 0.4, 1, 2]; % Integral time values
-Kc = 3; % Fixed controller gain
-simTime = 2; % Simulation time in seconds
+% set simulation settings
+tauI_values = [0.01, 0.05, 0.1, 0.4, 1, 2];
+Kc = 3;
+simTime = 2; 
 
-% Load the Simulink model
+% load model as system
 cd("Simulinks\")
 modelName = 'model1_7'; 
 load_system(modelName);
 cd("..");
 
-% Plot settings
+% plot
 figure;
 hold on;
 colors = lines(length(tauI_values)); 
 
-% Loop through each integral time value and run the simulation
+% loop through each integral time value and run the simulation
 for i = 1:length(tauI_values)
-    % Set the integral time constant in the PID Controller block
+    % set the integral time constant in the PID Controller block
     set_param([modelName '/PI Controller'], 'I', num2str(Kc/tauI_values(i)));
-    set_param([modelName '/PI Controller'], 'P', num2str(Kc)); % Ensure Kc is set
+    set_param([modelName '/PI Controller'], 'P', num2str(Kc)); 
     
-    % Run simulation
+    % run
     simOut = sim(modelName, 'StopTime', num2str(simTime));
     
-    % Access y(t) response
     response_data = simOut.response;
     time = response_data.time;  
     y_t = response_data.signals.values; 
     
-    % Plot response
+    % plot 
     plot(time, y_t, 'Color', colors(i, :), 'DisplayName', ['\tau_I = ' num2str(tauI_values(i))]);
 end
 
-% Formatting
 xlabel('Time (s)');
 ylabel('y(t)');
 title('Closed-Loop Response for Different Integral Time Constants (\tau_I)');
@@ -84,9 +82,54 @@ legend show;
 grid on;
 hold off;
 
-% Save the figure
 saveas(gcf, 'Figures/figure1_9.png');
-
-% Close the Simulink model without saving changes
 close_system(modelName, 0);
 
+%% Problem 1.10
+tau_p = 0.5;
+K_p = 3;
+
+Kc_values = linspace(0.05, 5, 100); % More points for smoother plots
+tauI_values = linspace(0.01, 2, 100);
+
+[Kc_grid, tauI_grid] = meshgrid(Kc_values, tauI_values);
+
+tau_grid = sqrt((tauI_grid * tau_p) ./ (Kc_grid * K_p));
+zeta_grid = 0.5 * (1 + Kc_grid * K_p) .* sqrt(tauI_grid ./ (Kc_grid * K_p * tau_p));
+
+[KC_mesh, tauI_mesh] = meshgrid(Kc_values, tauI_values);
+critical_plane = ones(size(KC_mesh)); 
+
+% plot surface plot of closed-loop time constant tau
+
+figure;
+surf(Kc_values, tauI_values, tau_grid);
+shading interp;
+xlabel('Kc');
+ylabel('\tau_I');
+zlabel('Time Constant \tau');
+title('Surface Plot of Closed-Loop Time Constant \tau');
+colorbar;
+grid on;
+view(3);
+saveas(gcf, 'Figures/figure1_10a.png');
+
+% plot surface plot of closed-loop damp factor
+
+figure;
+hold on;
+surf(Kc_values, tauI_values, zeta_grid);
+shading interp;
+xlabel('Kc');
+ylabel('\tau_I');
+zlabel('Damping Factor \zeta');
+title('Surface Plot of Closed-Loop Damping Factor \zeta');
+colorbar;
+grid on;
+view(3);
+
+surf(KC_mesh, tauI_mesh, critical_plane, 'FaceAlpha', 0.4, 'FaceColor', 'r', 'EdgeColor', 'none');
+
+legend({'\zeta Surface', 'Critical Damping Plane (\zeta = 1)'}, 'Location', 'best');
+hold off;
+saveas(gcf, 'Figures/figure1_10b.png');
